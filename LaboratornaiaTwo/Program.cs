fe8293a4e4eb4ea266bab7819919c043bc218491
openpersonaliti <66948281+openpersonaliti@users.noreply.github.com>
2020-09-24 21:19:15 +0500

    Добавьте файлы проекта.

---
+++ LaboratornaiaTwo/Program.cs
@@ -0,0 +1,15 @@
+﻿using SolutionName;
+
+
+
+namespace MainName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SolutionForLab objectSFL = new SolutionForLab();
+            objectSFL.SolutionTask();
+        }
+    }
+}
---
+++ LaboratornaiaTwo/SolutionForLab.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+
+
+namespace SolutionName
+{
+    public class SolutionForLab
+    {
+        private const int Three = 3,
+            K = 10;
+        static private int changeToB = 1,  
+            givenN = 0;
+        static private double xAxis,
+            valueSummForGivenN,
+            valueSummForGivenE,
+            changeFromA = 0.1,
+            precisionE = 0.0001,
+            amountSteps = (changeToB - changeFromA) / K,
+            checkPrecisionAndSummForVLSFE;
+
+
+        private int Factorial(int givenN)
+        {
+            if (givenN == 0)
+                return 0;
+            else if (givenN == 1)
+                return 1;
+
+            return givenN * Factorial(givenN - 1);
+        }
+
+        private double SummRow(int givenN, double xAxis)
+        {
+            double numeratorOfCommonMember = Math.Pow(Math.Log(Three), givenN);
+            double denominatorOfCommonMember = Factorial(givenN);
+            double multiplierOfCommonMember = Math.Pow(xAxis, givenN);
+
+            if (denominatorOfCommonMember == 0)
+                return 1.0;
+            else
+                return (numeratorOfCommonMember / denominatorOfCommonMember) * multiplierOfCommonMember;
+        }
+       
+        public void SolutionTask()
+        {
+            for(xAxis = changeFromA; xAxis <= changeToB; xAxis += amountSteps)
+            {
+                for (givenN = 0; givenN <= 10; givenN++)
+                {
+                    valueSummForGivenN += SummRow(givenN, xAxis);
+                }
+
+                givenN = 0;
+
+                do
+                {
+                    checkPrecisionAndSummForVLSFE = SummRow(givenN, xAxis);
+                    valueSummForGivenE += checkPrecisionAndSummForVLSFE;
+                    givenN++;
+
+                } while (checkPrecisionAndSummForVLSFE > precisionE);
+                
+                Console.WriteLine("X = " + (xAxis) + ";" + "\tSN = " + (valueSummForGivenN) + ";" + "\tSE = " + (valueSummForGivenE) + ";" 
+                    + 
+                    "\tY = " + (Math.Pow(Three, xAxis)) + ".\n");
+
+                valueSummForGivenN = 0;
+                valueSummForGivenE = 0;
+            }
+        }
+    }
+}
